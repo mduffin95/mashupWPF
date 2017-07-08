4539dde11f64d336c266cd88937c4bde1f5a0853
Matthew Duffin <matthewjduffin@gmail.com>
2017-07-08 23:38:51 +0100

    Drag and drop now working

--- Mashup/CompositionViewModel.cs
+++ Mashup/CompositionViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -17,6 +18,13 @@ namespace Mashup
         public ObservableCollection<SegmentViewModel> Segments
         {
             get { return _segments; }
+        }
+
+        private readonly ObservableCollection<SegmentViewModel> _searchSegments
+            = new ObservableCollection<SegmentViewModel>();
+        public ObservableCollection<SegmentViewModel> SearchSegments
+        {
+            get { return _searchSegments; }
         }
 
         private string _fileText;
@@ -40,5 +48,34 @@ namespace Mashup
             if (openFileDialog.ShowDialog() == true)
                 _fileText = File.ReadAllText(openFileDialog.FileName);
         }
+
+        private string _searchText;
+        public string SearchText
+        {
+            get { return _searchText; }
+            set
+            {
+                _searchText = value;
+                RaisePropertyChangedEvent(nameof(SearchText));
+            }
+        }
+
+        public ICommand SearchCommand
+        {
+            get { return new DelegateCommand(Search); }
+        }
+
+        private void Search()
+        {
+            string pattern = SearchText;
+            string sentence = _fileText;
+
+            SearchSegments.Clear();
+            foreach (Match match in Regex.Matches(sentence, pattern))
+            {
+                SearchSegments.Add(new SegmentViewModel(match.Index, match.Length, _fileText));
+            }
+        }
+
     }
 }
